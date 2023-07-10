770242795890c7791b5605e23ae97431b6297b75
Kolya <robbinbobin1@gmail.com>
2023-07-10 22:18:04 +0300

    Modified hw-4

--- js 4/index.js
+++ js 4/index.js
@@ -143,3 +143,8 @@ redBtn.addEventListener('highlight', function (e) { // Listen to the highlight e
 });
 
 highlight(redBtn); // highlight redBtn element
+
+let elementWithTitle = document.querySelector('h2'); // Селектор для елементу з текстом 'Навігація по DOM дереву'
+let firstSection = document.querySelector('section:first-of-type'); // Селектор для першого елементу <section>
+let listItem5 = document.querySelector('li:nth-child(5)'); // Селектор для елементу списку з текстом 'Пункт 5'
+let elementWithClass = document.querySelector('.hatredLevelBlock'); // Селектор для елементу з класом 'hatredLevelBlock'
